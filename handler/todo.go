8311b1c54008e2440a757ad5404d116714b1c919
aoi <149755797+Aoioa7@users.noreply.github.com>
2024-02-04 10:59:34 +0900

    ok

--- handler/todo.go
+++ handler/todo.go
@@ -71,6 +71,8 @@ func(h *TODOHandler) ServeHTTP(w http.ResponseWriter,r *http.Request){
 		err:=json.NewDecoder(r.Body).Decode(req)
 		if err!=nil{
 			log.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
 		}
 		if len(req.Subject)==0||req.ID==0{
 			w.WriteHeader(http.StatusBadRequest)
