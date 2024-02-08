dd479a5a25405ba23fb749f90dc1cc950307de37
aoi <149755797+Aoioa7@users.noreply.github.com>
2024-02-08 10:49:32 +0900

    ok

--- service/todo.go
+++ service/todo.go
@@ -3,6 +3,8 @@ package service
 import (
 	"context"
 	"database/sql"
+	"fmt"
+	"strings"
 
 	"github.com/TechBowl-japan/go-stations/model"
 )
@@ -129,6 +131,31 @@ func (s *TODOService) UpdateTODO(ctx context.Context, id int64, subject, descrip
 // DeleteTODO deletes TODOs on DB by ids.
 func (s *TODOService) DeleteTODO(ctx context.Context, ids []int64) error {
 	const deleteFmt = `DELETE FROM todos WHERE id IN (?%s)`
+	if len(ids)==0{
+		return nil
+	}
+	questions:=strings.Repeat(",?",len(ids)-1)
+	query:=fmt.Sprintf(deleteFmt,questions)
+	stmt,err:=s.db.PrepareContext(ctx,query)
+	if err!=nil{
+		return err
+	}
+	defer stmt.Close()
+	args:=make([]interface{},len(ids))
+	for i,id :=range ids{
+		args[i]=id
+	}
+	res,err:=stmt.ExecContext(ctx,args...)
+	if err!=nil{
+		return err
+	}
+	rowNum,err:=res.RowsAffected()
+	if err!=nil{
+		return nil
+	}
+	if rowNum==0{
+		return &model.ErrNotFound{}
+	}
 
-	return nil
+	return err
 }
